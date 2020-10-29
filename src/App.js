9bbe5ef52ad4ed240ec34578997486c92c059d86
Colman Currie <curriecolman@gmail.com>
2020-10-29 13:50:08 -0700

    refactor lists add key and index

--- src/App.js
+++ src/App.js
@@ -14,16 +14,11 @@ class App extends Component {
      showPersons: false
   }
 
-  switchNameHandler = (newName) => {
-    // this.state.persons[0].name = ; DONT DO This
-    this.setState({
-      persons: [
-        { name: newName, age: 30 },
-        { name: 'Colman', age: 31 },
-        { name: 'Bailey', age: 7 }
-  
-      ]
-    })
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.persons.slice(); EQUIVILANT to below method, below is more modern/up to date
+    const persons= [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
   }
 
   nameChangedHandler = (event) => {
@@ -54,20 +49,14 @@ class App extends Component {
 
     if (this.state.showPersons) {
       persons = (
-        < div >
-           <Person 
-             name= {this.state.persons[0].name} 
-             age= {this.state.persons[0].age}/>
-           <Person 
-             name= {this.state.persons[1].name} 
-             age= {this.state.persons[1].age}
-             click={this.switchNameHandler.bind(this, 'Anna!')} 
-             changed={this.nameChangedHandler}
-             >My Hobby is: Amateur Dog Handling</Person>
-           <Person 
-            name= {this.state.persons[2].name} 
-            age= {this.state.persons[2].age}/>
-        </div > 
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person 
+            click={() => this.deletePersonHandler(index)}
+            name={person.name}
+            age={person.age}/>
+          })}
+        </div> 
       );
     }
     return (
