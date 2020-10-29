b303f7d5a02f9dec1867444b4087cd84d230b081
Colman Currie <curriecolman@gmail.com>
2020-10-29 14:33:55 -0700

    find bug, it was spl error

--- src/App.js
+++ src/App.js
@@ -14,21 +14,30 @@ class App extends Component {
      showPersons: false
   }
 
+
+
+
   deletePersonHandler = (personIndex) => {
     // const persons = this.state.persons.slice(); EQUIVILANT to below method, below is more modern/up to date
-    const persons= [...this.state.persons];
+    const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
     this.setState({persons: persons});
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Anna', age: 30},
-        { name: event.target.value, age: 31},
-        { name: 'Bailey', age: 5}
-      ]
-    })
+  nameChangedHandler = ( event, id ) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+    const person = {
+      ...this.state.persons[personIndex]
+      };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState( {persons: persons} );
 
   }
 
@@ -55,7 +64,8 @@ class App extends Component {
             click={() => this.deletePersonHandler(index)}
             name={person.name}
             age={person.age}
-            key={person.id}/>
+            key={person.id}
+            changed={(event) => this.nameChangedHandler(event, person.id)} />
           })}
         </div> 
       );
